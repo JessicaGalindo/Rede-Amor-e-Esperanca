7e1233401ba59e8b565f7032a9175b8c20bec95e
jessica <jessicakgsantos1994@gmail.com>
2020-10-16 17:03:18 -0300

    arquivos dinamicos com node.js e hbs

--- src/server.js
+++ src/server.js
@@ -1,2 +1,22 @@
-const express = requie('express');
-const server = express();
+//importar pacotes
+const express = require("express");
+const path = require("path");
+
+//iniciando o express
+const server = express()
+server
+  //pasta de arquivos estaticos - cria a rota para o dinamico
+  .use(express.static("public"))
+
+  // configurar template engine
+  .set("views", path.join(__dirname, "views"))
+  .set('view engine', 'hbs')
+
+  //criar um caminho
+  .get("/", (request, response) => {
+    // / é o mesmo que index.html
+    return response.render('index')
+  })
+
+//ligar o servidor
+server.listen(5500)
