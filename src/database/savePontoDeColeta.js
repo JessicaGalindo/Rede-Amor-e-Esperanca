9bfd813b1b586b55e6ee9ca7bc15ed30d799af80
jessica <jessicakgsantos1994@gmail.com>
2020-10-18 17:36:59 -0300

    logo customizado, milha extra finalizada

--- src/database/savePontoDeColeta.js
+++ src/database/savePontoDeColeta.js
@@ -1,4 +1,8 @@
-function savePontoDeColeta(db, pontoDeColeta){
+function savePontoDeColeta(db, pontoDeColeta){ 
+    //trocar pontoDeColeta por {lat, lng, name, description, contato, images, instructions, opening_hours, open_on_weekends}
+
+    //{lat, lng, name, description, contato, images, instructions, opening_hours, open_on_weekends}){
+
     //insere dados na tabela
     //await permite que nao coloque then no final, pois ele espera a execução e ja vai para o proximo comando
     return db.run(`
@@ -23,7 +27,7 @@ function savePontoDeColeta(db, pontoDeColeta){
             '${pontoDeColeta.opening_hours}',
             '${pontoDeColeta.open_on_weekends}'
         );
-    `);
+    `); //retirar pontoDeColeta no VALUES
 };
 
 module.exports = savePontoDeColeta;
