8ce2f7ad297d6a35387e330d1f4fb3deb5ef07cb
jessica <jessicakgsantos1994@gmail.com>
2020-10-17 23:07:42 -0300

    mudança na borda do logo

--- src/pages.js
+++ src/pages.js
@@ -1,22 +1,33 @@
-const pontosDeColeta = require('./database/fakedata.js');
+//const pontosDeColeta = require('./database/fakedata.js');
+const Database = require("./database/db")
+const savePontoDeColeta = require("./database/savePontoDeColeta")
 
 module.exports = {
-    
-    index(request, response) {
-        // / é o mesmo que index.hbs
-        return response.render('index')
-    }, 
+  index(request, response) {
+    // / é o mesmo que index.hbs
+    return response.render("index");
+  },
 
-    ponto(request, response) {
-        return response.render('ponto')
-    }, 
+  ponto(request, response) {
+    return response.render("ponto");
+  },
 
-    pontosDeColeta(request, response) {
-        return response.render('pontos_de_coleta', {pontosDeColeta})
-    }, 
-
-    createPontoDeColeta(request, response) {
-        return response.render('create-ponto-de-coleta')
+  //deve colocar o async na frente por causa do await
+  async pontosDeColeta(request, response) {
+    try {
+      const db = await Database; //para acessar o database sem precisar usar o then
+      //colocar a variavel pelo banco de dados
+      const pontoDeColeta = await db.all(
+        'SELECT * FROM tabelaPontoDeColeta WHERE id = "2"'
+      )
+      return response.render("pontos_de_coleta", { pontosDeColeta })
+    } catch (error) {
+      console.log(error)
+      return res.send("Erro no banco de dados!")
     }
+  },
 
+  createPontoDeColeta(request, response) {
+    return response.render("create-ponto-de-coleta")
+  }
 }
